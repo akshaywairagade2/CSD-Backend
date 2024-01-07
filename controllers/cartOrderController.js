1631e959b431131577bebebf7e0ee51ab3e461bd
= <=>
2024-01-07 17:39:28 +0530

    fixed middleware/auth

--- controllers/cartOrderController.js
+++ controllers/cartOrderController.js
@@ -1,14 +1,15 @@
 const cartOrder  = require('../models/cartOrders') ;   
 const ErrorHandler = require('../utils/errorhandler');
 const catchAsyncError = require('../middlewares/catchAsyncError');
-const user = require('../models/user');
+const user = require('../models/user'); 
+const mongoose  = require('mongoose') ; 
 
 
 exports.addToCart  = catchAsyncError(  
        async(req , res , next)=>{    
         
             const {hotelID , item} =  req.body ; 
-            const userID =hotelID ;    
+            const userID = req.userID ;    
     
             const cart  = await cartOrder.findOne({userID: userID , hotelID: hotelID}) ; 
             if(!cart){ 
@@ -18,15 +19,11 @@ exports.addToCart  = catchAsyncError(
                      orderItems:[], 
                 })
             } 
-           try{
+          
             await cart.addItem(item) ;   
 
             res.status(200).send({success: true , message: "added Successfully" , ...cart}) ; 
-           }
-           catch(error){ 
-               return next(new ErrorHandler(error , 401)) ; 
-           }
-
+           
 
        }
 ) ; 
@@ -34,9 +31,9 @@ exports.addToCart  = catchAsyncError(
 exports.removeToCart  = catchAsyncError( 
     async(req , res , next)=>{ 
         const {hotelID , item} =  req.body ; 
-        const userID =  hotelID;    
+        const userID =  req.userID;    
  
-         const cart  = await cartOrder.findOne({userID: userID , hotelID: hotelID}) ; 
+         const cart  = await cartOrder.findOne({ userID: userID , hotelID: hotelID}) ; 
          if(!cart){ 
              await cartOrder.create({  
                   userID: userID, 
@@ -47,7 +44,7 @@ exports.removeToCart  = catchAsyncError(
         try{
          await cart.removeItem(item) ;   
 
-         res.status(200).send({success: true , message: "added Successfully" , cart: cart}) ; 
+         res.status(200).send({success: true , message: "removed Successfully" , cart: cart}) ; 
         }
         catch(error){ 
             return next(new ErrorHandler(error , 401)) ; 
@@ -61,16 +58,16 @@ exports.removeToCart  = catchAsyncError(
 exports.getCart =  async(req , res , next)=>{ 
     const hotelID = req.params.id ; 
     console.log(hotelID) ;  
-    const userID  = hotelID  ; 
+    const userID  = req.userID  ; 
     console.log(userID) ; 
-        const cart =  await cartOrder.findOne({userID: userID , hotelID: hotelID}) ;   
+        const cart =  await cartOrder.find({userID: userID , hotelID: hotelID}) ;   
     
-
+        console.log(cart) ; 
         if (!cart) {
             res.status(200).send({message:"cart not found" , cart: []}) ; 
         } 
         else  { 
-            res.status(200).send({message: "cart found" , cart: cart}); 
+            res.status(200).send({message: "cart found" , items: cart}); 
         }
     }
        
@@ -79,7 +76,7 @@ exports.getCart =  async(req , res , next)=>{
 
 exports.deleteCart =  catchAsyncError( async(req , res , next)=>{ 
     const hotelID = req.params.id ; 
-    const userID  = hotelID; 
+    const userID  = req.userID; 
 
     const cart =  await cartOrder.findOneAndDelete({userID: userID , hotelID: hotelID}) ; 
          
