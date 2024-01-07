6573d18534865e4d7834dd11e20c89aec309e555
= <=>
2024-01-07 16:33:09 +0530

    cart functionality added

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ app.use(cookiePareser()) ;
 app.use(express.json()) ; 
 
 
-const authRoutes = require('./routes/auth')
+const authRoutes = require('./routes/auth') 
+const cartRoutes = require('./routes/cartOrderRoutes') ;  
 app.use('/api/auth', authRoutes);
+app.use('/api/v1', cartRoutes) ; 
 
 const errorMiddleWare = require('./middlewares/error') ;     
 app.use(errorMiddleWare) ; 
--- models/cartOrders.js
+++ models/cartOrders.js
@@ -2,12 +2,12 @@ const mongoose =  require('mongoose') ;
 
 const cartOrderSchema =  new mongoose.Schema(
     {  
-       user : { 
+       userID : { 
          type:mongoose.Schema.ObjectId, 
          ref : "User",  
          required: true, 
        }, 
-       server :{ 
+       hotelID :{ 
         type:mongoose.Schema.ObjectId, 
         ref : "User",  
         required: true ,
@@ -26,8 +26,8 @@ const cartOrderSchema =  new mongoose.Schema(
               required: true, 
            }, 
            item: { 
-            type:mongoose.Schema.ObjectId, 
-            ref : "Item",
+            type: String, 
+            // ref : "Item",
             required: true , 
            }
        }] ,
@@ -36,6 +36,50 @@ const cartOrderSchema =  new mongoose.Schema(
 
 
     
-); 
+);   
+
+
+cartOrderSchema.methods.addItem = function(item) {
+   // Check if the item already exists in the cart
+   const existingItem = this.orderItems.find(orderItem => orderItem.item == (item.id));
+
+   if (existingItem) {
+       // If the item exists, update the quantity
+       existingItem.quantity += 1;
+   } else {
+       // If the item doesn't exist, add it to the cart 
+       console.log("creating a new item"); 
+       this.orderItems.push({
+           name: item.name,
+           price: item.price,
+           quantity: 1,
+           item: item.id
+       });
+   }
+
+   // Save the changes to the cart order
+   return this.save();
+};
+
+cartOrderSchema.methods.removeItem = function(item) {
+   // Find the index of the item in the orderItems array
+   const itemIndex = this.orderItems.findIndex(orderItem => orderItem.item == (item.id));
+
+   if (itemIndex !== -1) {
+       // If the item is found, decrease the quantity
+       this.orderItems[itemIndex].quantity -= 1;
+
+       // If the quantity becomes zero, remove the item from the cart
+       if (this.orderItems[itemIndex].quantity <= 0) {
+           this.orderItems.splice(itemIndex, 1);
+       }
+
+       // Save the changes to the cart order
+       return this.save();
+   }
+
+   // If the item is not found in the cart, return the cart order without changes
+   return Promise.resolve(this);
+};
 
 module.exports = mongoose.model('CartOrder' , cartOrderSchema) ; 
