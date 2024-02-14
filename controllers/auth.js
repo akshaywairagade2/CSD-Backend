712b9aeb6829b2a0cfbf6c74df1aca06ef904a38
Akshay <akshaywairagade2@gmail.com>
2024-02-14 17:24:33 +0530

    Added Frontend Link

--- controllers/auth.js
+++ controllers/auth.js
@@ -224,7 +224,7 @@ const verifymailsenderonaccountcreation = async (email, hashPassword, id) => {
         const token = jwt.sign({ email: email, id: id }, secret, {
             expiresIn: "4h"
         })
-        const link = `http://localhost:3000/verifymailonaccountcreation/${id}/${token}`;
+        const link = `https://iitbh-campus-delivery.netlify.app/verifymailonaccountcreation/${id}/${token}`;
         var transporter = await nodemailer.createTransport({
             service: 'gmail',
             auth: {
@@ -321,7 +321,7 @@ exports.forgotpassword = async (req, res) => {
             const token = jwt.sign({ email: user.emailId, id: user._id }, secret, {
                 expiresIn: "5m"
             })
-            const link = `http://localhost:3000/reset-password/${user._id}/${token}`;
+            const link = `https://iitbh-campus-delivery.netlify.app/reset-password/${user._id}/${token}`;
             var transporter = await nodemailer.createTransport({
                 service: 'gmail',
                 auth: {
