5d5a949c8288b076e3c42c1823da94351943f959
sushantyerawar1 <sushantdyerawar@gmail.com>
2024-04-10 00:34:21 +0530

    Added ratelimiter features

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const rateLimiter = rateLimit({
     windowMs: 60 * 1000,
-    limit: 100,
+    limit: 10,
     standardHeaders: 'draft-7',
     legacyHeaders: false,
     message: 'Too many requests from this IP, please try again later'
